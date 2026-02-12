8963792e71ca6cd16a88327a45db919f994fd85f
Syed Mustafa Quadri <syedmustafaquadri32@gmail.com>
2026-02-12 17:43:48 +0530

    Implement deployment endpoint in server.js
    Added a POST endpoint to trigger deployment via git pull and npm install.

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const { exec } = require("child_process");
 const app = express();
 const PORT = 3001;
 
 app.get("/", (req, res) => res.send("Hello World! App is running on Termux!"));
+
+app.post("/deploy", (req, res) => {
+  console.log("Deploy triggered...");
+
+  exec("git pull origin main && npm install", (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error during deploy: ${err}`);
+      return res.status(500).send(`Deploy failed: ${err.message}`);
+    }
+
+    console.log(stdout);
+    console.error(stderr);
+
+    res.send("Code updated! Restarting server...");
+
+
+  });
+});
 
 app.listen(PORT, () => console.log(`App server running on port ${PORT}`));
